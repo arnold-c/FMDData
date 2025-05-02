46affe3d2c9edd2ede37a098452245cb6d8568d7
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-02 10:14:48 -0600

    Add test for renaming aggregated pre/post counts

--- test/icar-cleaning-functions.jl
+++ test/icar-cleaning-functions.jl
@@ -1,23 +1,6 @@
 using DataFrames
 
 @testset verbose = true "icar-cleaning-functions.jl" begin
-    #    clean_colnames,
-    #    rename_aggregated_pre_post_counts,
-    #    correct_all_state_names,
-    #    check_duplicated_columns,
-    #    check_duplicated_states,
-    #    check_allowed_serotypes,
-    #    check_pre_post_exists,
-    #    has_totals_row,
-    #    all_totals_check,
-    #    calculate_state_counts,
-    #    calculate_state_seroprevalence
-    #
-    # collect_all_present_serotypes,
-    #    check_aggregated_pre_post_counts_exist,
-    #    contains_seroprev_results,
-    #    contains_count_results,
-    #    correct_state_name
 
     @testset "Load CSV" begin
         filename = "missing-data.csv"
@@ -89,19 +72,16 @@ using DataFrames
 
     end
 
+    dir = "./"
+    filename = "test-data.csv"
+    data = load_csv(
+        filename,
+        dir,
+        DataFrame
+    )
+    cleaned_data = clean_colnames(data)
+
     @testset "Column name cleaning" begin
-        dir = "./"
-
-        filename = "test-data.csv"
-
-        data = load_csv(
-            filename,
-            dir,
-            DataFrame
-        )
-
-        cleaned_data = clean_colnames(data)
-
         @test isequal(
             names(cleaned_data),
             [
@@ -130,6 +110,41 @@ using DataFrames
                 AssertionError("[\"flag-this_column^\"] are columns with disallowed characters.\nDict{String, Vector{RegexMatch}}(\"flag-this_column^\" => [RegexMatch(\"-\"), RegexMatch(\"^\")])")
             )
         end
-
     end
+
+    renamed_aggregated_counts_df = rename_aggregated_pre_post_counts(cleaned_data)
+    @testset "Rename aggregated Pre/Post counts" begin
+        @test isequal(
+            names(renamed_aggregated_counts_df),
+            [
+                "states_ut",
+                "serotype_all_count_pre",
+                "serotype_all_count_post",
+                "serotype_o_pct_pre",
+                "serotype_o_pct_post",
+                "serotype_a_pct_pre",
+                "serotype_a_pct_post",
+                "serotype_asia1_pct_pre",
+                "serotype_asia1_pct_post",
+            ]
+        )
+    end
+
+    #    rename_aggregated_pre_post_counts,
+    #    correct_all_state_names,
+    #    check_duplicated_columns,
+    #    check_duplicated_states,
+    #    check_allowed_serotypes,
+    #    check_pre_post_exists,
+    #    has_totals_row,
+    #    all_totals_check,
+    #    calculate_state_counts,
+    #    calculate_state_seroprevalence
+    #
+    # collect_all_present_serotypes,
+    #    check_aggregated_pre_post_counts_exist,
+    #    contains_seroprev_results,
+    #    contains_count_results,
+    #    correct_state_name
+
 end
