7c076c9b6e83e07e346c4c71360107dcf2601ed5
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-02 09:36:02 -0600

    Add tests for loading csv files

---
+++ test/icar-cleaning-functions.jl
@@ -0,0 +1,92 @@
+using DataFrames
+
+@testset verbose = true "icar-cleaning-functions.jl" begin
+    # load_csv,
+    #    clean_colnames,
+    #    rename_aggregated_pre_post_counts,
+    #    correct_all_state_names,
+    #    check_duplicated_columns,
+    #    check_duplicated_states,
+    #    check_allowed_serotypes,
+    #    check_pre_post_exists,
+    #    has_totals_row,
+    #    all_totals_check,
+    #    calculate_state_counts,
+    #    calculate_state_seroprevalence
+    #
+    # collect_all_present_serotypes,
+    #    check_aggregated_pre_post_counts_exist,
+    #    contains_seroprev_results,
+    #    contains_count_results,
+    #    correct_state_name
+
+    @testset "Load CSV" begin
+        filename = "missing-data.csv"
+        not_a_dir = "./not-a-dir.txt"
+
+        @test try
+            load_csv("missing-data.tsv", not_a_dir)
+        catch e
+            isequal(e, ErrorException("$not_a_dir is not a valid directory"))
+        end
+
+        dir = "./"
+
+        @test try
+            load_csv(filename, dir)
+        catch e
+            isequal(e, ErrorException("$filename is not within the directory $dir"))
+        end
+
+        filename = "test-data.csv"
+
+        data = load_csv(
+            filename,
+            dir,
+            DataFrame
+        )
+
+        @test isequal(
+            typeof(data),
+            DataFrame
+        )
+
+        expected_col_names = [
+            "States/UT",
+            "Pre (N)",
+            "Post (N)",
+            "Serotype O (%) Pre",
+            "Serotype O (%) Post",
+            "Serotype A (%) Pre",
+            "Serotype A (%) Post",
+            "Serotype Asia1 (%) Pre",
+            "Serotype Asia1 (%) Post",
+        ]
+
+        expected_col_types = Dict(
+            zip(
+                expected_col_names, [
+                    AbstractString,
+                    Union{Missing, Integer},
+                    Union{Missing, Integer},
+                    Union{Missing, AbstractFloat},
+                    Union{Missing, AbstractFloat},
+                    Union{Missing, AbstractFloat},
+                    Union{Missing, AbstractFloat},
+                    Union{Missing, AbstractFloat},
+                    Union{Missing, AbstractFloat},
+                ]
+            )
+        )
+
+        @test isequal(
+            names(data),
+            expected_col_names
+        )
+
+        for (j, col) in pairs(eachcol(data))
+            @test eltype(col) <: expected_col_types[String(j)]
+        end
+
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,14 @@ using Test
 using JET
 import Aqua
 
-@testset "FMDData" begin
-    @testset "Static analysis with JET.jl" begin
-        @test isempty(JET.get_reports(report_package(FMDData, target_modules = (FMDData,))))
-    end
+@testset verbose = true "FMDData" begin
+    # @testset "Static analysis with JET.jl" begin
+    #     @test isempty(JET.get_reports(report_package(FMDData, target_modules = (FMDData,))))
+    # end
+    #
+    # @testset "QA with Aqua" begin
+    #     Aqua.test_all(FMDData)
+    # end
 
-    @testset "QA with Aqua" begin
-        Aqua.test_all(FMDData)
-    end
-
-    # include("../test/icar-cleaning-functions.jl")
+    include("../test/icar-cleaning-functions.jl")
 end
