d4b361f3b7033c110d43d8b738598327bd75f29b
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-02 18:44:13 -0600

    Update _check_similar_column_names to check for substrings not string distances
    Coercion by df to make unique columns will append numbers/chars, and
    string distances will flag the same type of column for different
    serotype e.g. pre pcts.

--- src/icar-cleaning-functions.jl
+++ src/icar-cleaning-functions.jl
@@ -164,13 +164,9 @@ end
 
 Wrapper function around the two internal functions `_check_identical_column_names()` and `_check_similar_column_names()`. If a DataFrame is created then all identical column names should result in an error before it is created, but potentially they may be coerced to be made unique so a similarity check should be performed.
 """
-function check_duplicated_column_names(
-        df::DataFrame,
-        metric::T = Levenshtein();
-        min_score = 0.79
-    ) where {T <: Union{<:Metric, <:SemiMetric}}
+function check_duplicated_column_names(df::DataFrame)
     _check_identical_column_names(df)
-    _check_similar_column_names(df, metric; min_score = min_score)
+    _check_similar_column_names(df)
     return nothing
 end
 
@@ -199,27 +195,40 @@ function _check_identical_column_names(df::DataFrame)
 end
 
 """
-    _check_similar_column_names(
-        df::DataFrame,
-        metric::T = Levenshtein();
-        min_score = 0.79
-    ) where {T <: Union{<:Metric, <:SemiMetric}}
+    _check_similar_column_names(df::DataFrame) where {T <: Union{<:Metric, <:SemiMetric}}
 
-Check if any columns have similar names.
+Check if any columns have similar names. Calculates if any column names are substrings of other columns names.
 
 Should be run AFTER `_check_identical_column_names()` as `push!()` call will overwrite previous Dict entry key if there are exact matches.
 """
-function _check_similar_column_names(
-        df::DataFrame,
-        metric::T = Levenshtein();
-        min_score = 0.79
-    ) where {T <: Union{<:Metric, <:SemiMetric}}
-    colnames = String.(names(df))
+function _check_similar_column_names(df::DataFrame)
+    colnames = sort(String.(names(df)); by = length)
     duplicates = Dict{String, Vector{String}}()
     for (i, nm) in pairs(colnames)
-        idx = findall(nm, @view(colnames[(i + 1):end]), metric, min_score = min_score)
-        if !isempty(idx)
-            push!(duplicates, nm => colnames[(i .+ idx)])
+        for (_, next_nm) in pairs(colnames[(i + 1):end])
+            if nm == next_nm
+                error("Has duplicate names. Run the function _check_identical_column_names() before running this function")
+            end
+            if contains(next_nm, nm)
+                if haskey(duplicates, nm)
+                    push!(duplicates[nm], next_nm)
+                else
+                    duplicates[nm] = [next_nm]
+                end
+            end
+            if contains(nm, next_nm)
+                if haskey(duplicates, next_nm)
+                    push!(duplicates[next_nm], nm)
+                else
+                    duplicates[next_nm] = [nm]
+                end
+            end
+        end
+    end
+
+    for k in keys(duplicates)
+        if in(k, reduce(vcat, values(duplicates)))
+            pop!(duplicates, k)
         end
     end
     if !isempty(duplicates)
