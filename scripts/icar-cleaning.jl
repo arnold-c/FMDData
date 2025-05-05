58fc0fee403f04eb1a1e1f5b56a69f8e28bd69cf
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-05 00:31:46 -0600

    Start cleaning data files
    Need to get logging to record Try errors for a clear log of all changes
    made to a data file during cleaning.
    
    Need to implement special checks for organized farms as some include
    farm-specific data that should be grouped into state-level data.

--- scripts/icar-cleaning.jl
+++ scripts/icar-cleaning.jl
@@ -3,81 +3,87 @@ using DrWatson
 @quickactivate "FMDData"
 
 using FMDData
-using CSV
-using DataFrames
+using Try
+using Logging, LoggingExtras
+
+# "2019_Annual-Report_A-N-Islands.csv"
+# "2019_Annual-Report_Andhra-Pradesh.csv"
+# "2019_Annual-Report_Bihar.csv"
+# "2019_Annual-Report_Chhattisgarh.csv"
+# "2019_Annual-Report_Goa.csv"
+# "2019_Annual-Report_Gujarat.csv"
+# "2019_Annual-Report_Haryana.csv"
+# "2019_Annual-Report_Jammu-Kashmir.csv"
+# "2019_Annual-Report_Karnataka.csv"
+# "2019_Annual-Report_Kerala.csv"
+# "2019_Annual-Report_Madhya-Pradesh.csv"
+# "2019_Annual-Report_Maharashtra.csv"
+# "2019_Annual-Report_Manipur.csv"
+# "2019_Annual-Report_Mizoram.csv"
+# "2019_Annual-Report_Odisha.csv"
+# "2019_Annual-Report_Pondicherry.csv"
+# "2019_Annual-Report_Punjab.csv"
+# "2019_Annual-Report_Rajasthan.csv"
+# "2019_Annual-Report_Tamil-Nadu.csv"
+# "2019_Annual-Report_Telangana.csv"
+# "2019_Annual-Report_Uttar-Pradesh.csv"
+# "2019_Annual-Report_Uttarakhand.csv"
+# "2019_Annual-Report_West-Bengal.csv"
+# "2020_Annual-Report_NADCP-1.csv"
+# "2020_Annual-Report_Organized-farms.csv"
+# "2021_Annual-Report_NADCP-1.csv"
+# "2021_Annual-Report_NADCP-2.csv"
+# "2021_Annual-Report_Organized-farms.csv"
+# "2022_Annual-Report_NADCP-2.csv"
+# "2022_Annual-Report_NADCP-3.csv"
+# "2022_Annual-Report_Organized-farms.csv"
 
 #%%
-nadcp_2 = load_csv(
-    "2022_Annual-Report_NADCP-2.csv",
-    icar_inputs_dir()
-) |>
-    clean_colnames
-
-#%%
-has_totals_row(nadcp_2)
-
-correct_all_state_names(nadcp_2)
-
-check_duplicated_states(nadcp_2)
-
-aggregated_nadcp_2 = rename_aggregated_pre_post_counts(nadcp_2)
-
-#%%
-all_totals_check(aggregated_nadcp_2)
-
-#%%
-calculated_count_data = calculate_state_counts(aggregated_nadcp_2)
-
-calculated_count_no_seroprev_data = rename(
-    s -> replace(s, r"(.*)_calculated$" => s"\1"),
-    select(calculated_count_data, Not(r"serotype_.*\(%\).*")),
+all_cleaning_steps(
+    "2022_Annual-Report_Organized-farms.csv",
+    icar_inputs_dir(),
 )
 
-calculated_seroprev_data = calculate_state_seroprevalence(calculated_count_no_seroprev_data)
-
-
-function compare_calculations(calculated_count_data, calculated_seroprev_data)
-    strp_count_data =
-        rename(
-        s -> replace(s, r"(.*)_calculated$" => s"\1"),
-        calculated_count_data
-    )
-
-    strp_seroprev_data = select(
-        rename(
-            s -> replace(s, r"(.*)_calculated$" => s"\1"),
-            calculated_seroprev_data
-        ), names(strp_count_data)
-    )
-
-    @show strp_count_data == strp_seroprev_data && return
-
-    @assert names(strp_count_data) == names(strp_seroprev_data)
-
-    for j in 2:ncol(strp_count_data)
-        println(hcat(strp_count_data[!, [1, j]], strp_seroprev_data[!, j]))
-    end
-    return
-end
-
-
-compare_calculations(calculated_count_data, calculated_seroprev_data)
-
+#%%
+all_cleaning_steps(
+    "2022_Annual-Report_NADCP-3.csv",
+    icar_inputs_dir(),
+)
 
 #%%
-# runic: off
-# clean column names
-# correct state names
-# check no duplicated states
-# assert counts are ints and seroprevs are floats (use dispatch later)
-# if total pre/post counts & serotype counts, calculate serotype seroprevs
-	# if serotype seroprevs exist, assert approx equal calculated serotype seroprevs
-# if total pre/post counts & serotype seroprovs, calculate state counts
-# if no total pre/post counts
-	# if serotype counts (calculated or provided), calculate total pre/post counts
-	# else warn
-# check if has totals row
-	# if yes, check if totals are correct (requires calculated serotype counts for weighted seroprev totals)
-		# if yes, remove totals row
-			# else, error
-# runic: on
+all_cleaning_steps(
+    "2022_Annual-Report_NADCP-2.csv",
+    icar_inputs_dir(),
+)
+
+#%%
+# errors
+all_cleaning_steps(
+    "2021_Annual-Report_Organized-farms.csv",
+    icar_inputs_dir(),
+)
+
+#%%
+all_cleaning_steps(
+    "2021_Annual-Report_NADCP-2.csv",
+    icar_inputs_dir(),
+)
+
+#%%
+all_cleaning_steps(
+    "2021_Annual-Report_NADCP-1.csv",
+    icar_inputs_dir(),
+)
+
+#%%
+# errors
+all_cleaning_steps(
+    "2020_Annual-Report_Organized-farms.csv",
+    icar_inputs_dir(),
+)
+
+#%%
+all_cleaning_steps(
+    "2020_Annual-Report_NADCP-1.csv",
+    icar_inputs_dir(),
+)
