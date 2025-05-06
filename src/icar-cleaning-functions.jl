4e6edf0c50b69311effb363211152bc3d1f02478
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-06 16:37:59 -0600

    Add more unit tests for select_calculated_cols!

--- src/icar-cleaning-functions.jl
+++ src/icar-cleaning-functions.jl
@@ -1063,7 +1063,7 @@ end
 Checks if the data contains both provided and calculated columns that refer to the same variables. If the calculated column is a % seroprevalence, keep the calculated values. If the calculated column is a column of counts, keep the provided as they are deemed to be more accurate (counts require no calculation and should be a direct recording/reporting of the underlying data). The cleaning function `check_calculated_values_match_existing()` should have been run before to ensure there are no surprises during this processing step i.e., accidentally deleting columns that should be retained.
 """
 function select_calculated_cols!(
-        df::DataFrame,
+        df::DataFrame;
         allowed_serotypes = vcat("all", default_allowed_serotypes),
         reg::Regex = Regex(
             "serotype_(?:$(join(allowed_serotypes, "|")))_(count|pct)_(?:pre|post)\$"
@@ -1072,11 +1072,13 @@ function select_calculated_cols!(
     colnames = names(df)
     all_matched_cols = filter(!isnothing, match.(reg, colnames))
 
+    length(all_matched_cols) > 0 || return Try.Err("No columns were matched by the regex. Check it correctly identifies the appropriate serotype data columns")
+
     for col in all_matched_cols
         colnm = col.match
         colcap = col.captures
 
-        length(colcap) == 1 || return Try.Err("Only 1 capture group should exist for the match $colnm. Found $(length(colcap)): $colcap.")
+        length(colcap) == 1 || return Try.Err("Only 1 capture group should exist for the column $colnm. Found $(length(colcap)): $colcap.")
         colcap = colcap[1]
         colcap in ["count", "pct"] || return Try.Err("The capture group is not expected. It should be one of [\"count\", \"pct\"], but instead it is $colcap")
 
--- test/icar-cleaning-functions.jl
+++ test/icar-cleaning-functions.jl
@@ -594,6 +594,70 @@ using Try
             ]
         )
 
+        @test isequal(
+            select_calculated_cols!(
+                DataFrame(
+                    "states_ut" => ["a", "b", "c", "total"],
+                    "serotype_all_count_pre" => [10, 10, 10, 30],
+                    "serotype_all_count_post" => [10, 10, 10, 30],
+                    "serotype_a_count_pre" => [2, 1, 1, 4],
+                    "serotype_a_count_post" => [8, 6, 5, 19],
+                    "serotype_a_pct_pre" => [20.0, 10.0, 10.0, 13.3],
+                    "serotype_a_pct_post" => [80.0, 60.0, 50.0, 63.3],
+                    "serotype_a_count_pre_calculated" => [2, 1, 1, 4],
+                    "serotype_a_pct_pre_calculated" => [20.0, 10.0, 10.0, 13.3],
+                );
+                reg = Regex(
+                    "serotype_(?:$(join(["all", "a"], "|")))_(count|pct)_(testing)_(?:pre|post)\$"
+                )
+            ),
+            Try.Err("No columns were matched by the regex. Check it correctly identifies the appropriate serotype data columns")
+        )
+
+        @test isequal(
+            select_calculated_cols!(
+                DataFrame(
+                    "states_ut" => ["a", "b", "c", "total"],
+                    "serotype_all_count_pre" => [10, 10, 10, 30],
+                    "serotype_all_count_post" => [10, 10, 10, 30],
+                    "serotype_a_count_testing_pre" => [2, 1, 1, 4],
+                    "serotype_a_count_post" => [8, 6, 5, 19],
+                    "serotype_a_pct_pre" => [20.0, 10.0, 10.0, 13.3],
+                    "serotype_a_pct_post" => [80.0, 60.0, 50.0, 63.3],
+                    "serotype_a_count_pre_calculated" => [2, 1, 1, 4],
+                    "serotype_a_pct_pre_calculated" => [20.0, 10.0, 10.0, 13.3],
+                );
+                reg = Regex(
+                    "serotype_(?:$(join(["all", "a"], "|")))_(count|pct)_(testing)_(?:pre|post)\$"
+                )
+            ),
+            Try.Err(
+                "Only 1 capture group should exist for the column serotype_a_count_testing_pre. Found 2: Union{Nothing, SubString{String}}[\"count\", \"testing\"]."
+            )
+        )
+
+        @test isequal(
+            select_calculated_cols!(
+                DataFrame(
+                    "states_ut" => ["a", "b", "c", "total"],
+                    "serotype_all_count_pre" => [10, 10, 10, 30],
+                    "serotype_all_count_post" => [10, 10, 10, 30],
+                    "serotype_a_testing_pre" => [2, 1, 1, 4],
+                    "serotype_a_count_post" => [8, 6, 5, 19],
+                    "serotype_a_pct_pre" => [20.0, 10.0, 10.0, 13.3],
+                    "serotype_a_pct_post" => [80.0, 60.0, 50.0, 63.3],
+                    "serotype_a_count_pre_calculated" => [2, 1, 1, 4],
+                    "serotype_a_pct_pre_calculated" => [20.0, 10.0, 10.0, 13.3],
+                );
+                reg = Regex(
+                    "serotype_(?:$(join(["all", "a"], "|")))_(testing)_(?:pre|post)\$"
+                )
+            ),
+            Try.Err(
+                "The capture group is not expected. It should be one of [\"count\", \"pct\"], but instead it is testing"
+            )
+        )
+
     end
 
 end
