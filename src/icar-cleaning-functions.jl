a25bdbd7fc3a6c006661b534ad40c7ecf000940d
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-05-19 17:37:15 -0600

    Start to add wrapper cleaning function specific to 2019 report tables
    2019 report tables are state-specific and do not have a lot of the
    summary rows/columns in the later report tables e.g., total rows and/or
    aggregated count columns

--- src/icar-cleaning-functions.jl
+++ src/icar-cleaning-functions.jl
@@ -10,6 +10,7 @@ using LoggingExtras
 using Preferences: @load_preference
 
 export all_cleaning_steps,
+    all_2019_cleaning_steps,
     load_csv,
     clean_colnames,
     rename_aggregated_pre_post_counts,
@@ -140,6 +141,111 @@ function all_cleaning_steps(
 
         _log_try_error(
             write_csv(output_filename, output_dir, calculated_state_seroprevs_data)
+        )
+    end
+
+    if filesize(logfile) == 0
+        rm(logfile)
+    end
+
+    return Try.Ok("Cleaning of $input_filename successful. Written to $output_filename.")
+end
+
+function all_2019_cleaning_steps(
+        input_filename::T1,
+        input_dir::T1;
+        output_filename::T1 = "clean_$input_filename",
+        output_dir::T1 = icar_cleaned_dir(),
+        load_format = DataFrame
+    ) where {T1 <: AbstractString}
+
+    if show_warnings
+        println("\n==========================================================================")
+        println("Cleaning $(joinpath(input_dir, input_filename))\n")
+    end
+
+    if !isdir(output_dir)
+        mkpath(output_dir)
+    end
+    logpath = joinpath(output_dir, "logfiles")
+    if !isdir(logpath)
+        mkpath(logpath)
+    end
+
+    filebase = match(r"(.*)\.csv", input_filename).captures[1]
+    logfile = joinpath(output_dir, "logfiles", "$filebase.log")
+    logger = FileLogger(logfile)
+
+    with_logger(logger) do
+        data = _log_try_error(
+            load_csv(
+                input_filename,
+                input_dir,
+                load_format
+            )
+        )
+        cleaned_colnames_data = _log_try_error(clean_colnames(data))
+        renamed_aggregate_counts_data = _log_try_error(
+            rename_aggregated_pre_post_counts(cleaned_colnames_data)
+        )
+        corrected_state_name_data = _log_try_error(
+            correct_all_state_names(renamed_aggregate_counts_data)
+        )
+
+        _log_try_error(check_duplicated_column_names(corrected_state_name_data))
+        _log_try_error(check_missing_states(corrected_state_name_data))
+        _log_try_error(check_allowed_serotypes(corrected_state_name_data), :Warn)
+        _log_try_error(check_seroprevalence_as_pct(corrected_state_name_data))
+        aggregated_counts_exist = check_aggregated_pre_post_counts_exist(corrected_state_name_data)
+        _log_try_error(aggregated_counts_exist, :Warn)
+        _log_try_error(check_pre_post_exists(corrected_state_name_data))
+
+        has_totals = has_totals_row(corrected_state_name_data)
+        totals_dict = calculate_all_totals(corrected_state_name_data)
+        _log_try_error(totals_dict, :Warn)
+        if Try.iserr(has_totals)
+            _log_try_error(has_totals, :Warn)
+        end
+
+        if Try.isok(totals_dict)
+            totals_check_state = all_totals_check(Try.unwrap(totals_dict), corrected_state_name_data)
+
+            if Try.iserr(totals_check_state)
+                _log_try_error(
+                    totals_check_state,
+                    :Warn
+                )
+                push!(
+                    corrected_state_name_data,
+                    merge(Dict("states_ut" => "Total calculated"), totals_dict);
+                    promote = true
+                )
+            end
+        end
+
+
+        out_df = corrected_state_name_data
+        if Try.isok(aggregated_counts_exist)
+            out_df = calculate_state_counts(out_df)
+            out_df = calculate_state_seroprevalence(out_df)
+        end
+
+        _log_try_error(
+            check_calculated_values_match_existing(out_df)
+        )
+
+        if Try.isok(totals_dict)
+            _log_try_error(select_calculated_totals!(out_df))
+        end
+        if Try.isok(aggregated_counts_exist)
+            _log_try_error(select_calculated_cols!(out_df))
+        end
+
+        _log_try_error(sort_columns!(out_df))
+        _log_try_error(sort_states!(out_df))
+
+        _log_try_error(
+            write_csv(output_filename, output_dir, out_df)
         )
     end
 
