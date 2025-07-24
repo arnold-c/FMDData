eb3743a0c24e45fb3dbedf61d2ca9c12189159ad
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-07-23 22:01:34 -0400

    Move precompilation steps to separate script

--- src/FMDData.jl
+++ src/FMDData.jl
@@ -27,75 +27,15 @@ include("./icar-processing/metadata-operations.jl")
 include("./icar-processing/data-inference.jl")
 include("./icar-processing/dataframe-operations.jl")
 
-
-using PrecompileTools: @setup_workload, @compile_workload
-using DrWatson: srcdir
 using Preferences: set_preferences!, delete_preferences!
 
-# Don't run precompilation steps if in temporary test environment
-delete_preferences!(FMDData, "show_warnings"; force = true)
-set_preferences!(FMDData, "show_warnings" => false)
-if isdir(icar_inputs_dir())
+# Precompilation
+# include("./precompilation.jl")
 
-    @setup_workload begin
-        @compile_workload begin
-            all_cleaning_steps(
-                "2022_Annual-Report_NADCP-2.csv",
-                icar_inputs_dir();
-                output_dir = srcdir()
-            )
-
-            all_cleaning_steps(
-                "2021_Annual-Report_NADCP-2.csv",
-                icar_inputs_dir();
-                output_dir = srcdir()
-            )
-
-            cumulative_nadcp_2_2022 = FMDData._log_try_error(
-                load_csv(
-                    "clean_2022_Annual-Report_NADCP-2.csv",
-                    srcdir()
-                )
-            )
-
-            nadcp_2_2021 = FMDData._log_try_error(
-                load_csv(
-                    "clean_2021_Annual-Report_NADCP-2.csv",
-                    srcdir()
-                )
-            )
-
-            nadcp_2_2022 = FMDData._log_try_error(
-                infer_later_year_values(
-                    cumulative_nadcp_2_2022,
-                    nadcp_2_2021,
-                )
-            )
-
-            FMDData._log_try_error(
-                add_all_metadata!(
-                    cumulative_nadcp_2_2022 => OrderedDict(
-                        :sample_year => "Combined",
-                        :report_year => 2022,
-                        :round_name => "NADCP 2",
-                        :test_type => "SPCE",
-                        :test_threshold => "1.65 log10 @ 35% inhibition"
-                    )
-                )
-            )
-        end
-        # Clean up files from precompile steps
-        dir_files = filter(t -> contains(t, r".*\.csv$"), readdir(srcdir()))
-        for file in dir_files
-            rm(srcdir(file))
-        end
-        rm(srcdir("logfiles"); recursive = true)
-    end
-end
-
-# Reset preferences to show warnings during package use
+# reset warnings preferences after precompilation steps
 delete_preferences!(FMDData, "show_warnings"; force = true)
 set_preferences!(FMDData, "show_warnings" => true)
+
 
 # Include to help LSP work in files outside of the src/ dir
 @static if false
