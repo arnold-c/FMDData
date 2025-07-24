cb7a065c0d2525b187c95add776b2fa3e7469bd8
Callum Arnold <24391445+arnold-c@users.noreply.github.com>
2025-07-23 22:01:34 -0400

    Update processing script to use Try.@? to return errors when necessary

--- scripts/icar-additional-processing.jl
+++ scripts/icar-additional-processing.jl
@@ -46,151 +46,181 @@ isdir(icar_processed_logdir) || mkpath(icar_processed_logdir)
 logfile = joinpath(icar_processed_logdir, "nadcp_2.log")
 logger = FileLogger(logfile)
 
-with_logger(logger) do
-    cumulative_nadcp_2_2022 = FMDData._log_try_error(
-        load_csv(
-            "clean_2022_Annual-Report_NADCP-2.csv",
-            icar_cleaned_dir()
-        )
-    )
+cumulative_nadcp_2_2022 = Try.@? FMDData._log_try_error(
+    load_csv(
+        "clean_2022_Annual-Report_NADCP-2.csv",
+        icar_cleaned_dir()
+    );
+    logger = logger
+)
 
-    nadcp_2_2021 = FMDData._log_try_error(
-        load_csv(
-            "clean_2021_Annual-Report_NADCP-2.csv",
-            icar_cleaned_dir()
-        )
-    )
+nadcp_2_2021 = Try.@? FMDData._log_try_error(
+    load_csv(
+        "clean_2021_Annual-Report_NADCP-2.csv",
+        icar_cleaned_dir()
+    );
+    logger = logger
+)
 
-    nadcp_2_2022 = FMDData._log_try_error(
-        infer_later_year_values(
-            cumulative_nadcp_2_2022,
-            nadcp_2_2021,
-        )
-    )
+nadcp_2_2022 = Try.@? FMDData._log_try_error(
+    infer_later_year_values(
+        cumulative_nadcp_2_2022,
+        nadcp_2_2021,
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            cumulative_nadcp_2_2022 => OrderedDict(
-                :sample_year => "2021/2022",
-                :report_year => 2022,
-                :round_name => "NADCP 2",
-                :test_type => "SPCE",
-                :test_threshold => "1.65 log10 @ 35% inhibition"
-            )
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        cumulative_nadcp_2_2022 => OrderedDict(
+            :sample_year => "2021/2022",
+            :report_year => 2022,
+            :round_name => "NADCP 2",
+            :test_type => "SPCE",
+            :test_threshold => "1.65 log10 @ 35% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            nadcp_2_2022 => OrderedDict(
-                :sample_year => 2022,
-                :report_year => 2022,
-                :round_name => "NADCP 2",
-                :test_type => "SPCE",
-                :test_threshold => "1.65 log10 @ 35% inhibition"
-            )
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        nadcp_2_2022 => OrderedDict(
+            :sample_year => 2022,
+            :report_year => 2022,
+            :round_name => "NADCP 2",
+            :test_type => "SPCE",
+            :test_threshold => "1.65 log10 @ 35% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            nadcp_2_2021 => OrderedDict(
-                :sample_year => 2021,
-                :report_year => 2021,
-                :round_name => "NADCP 2",
-                :test_type => "SPCE",
-                :test_threshold => "1.65 log10 @ 35% inhibition"
-            )
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        nadcp_2_2021 => OrderedDict(
+            :sample_year => 2021,
+            :report_year => 2021,
+            :round_name => "NADCP 2",
+            :test_type => "SPCE",
+            :test_threshold => "1.65 log10 @ 35% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    nadcp_2 = FMDData._log_try_error(
-        combine_round_dfs(
-            cumulative_nadcp_2_2022, nadcp_2_2022, nadcp_2_2021
-        )
-    )
+nadcp_2 = Try.@? FMDData._log_try_error(
+    combine_round_dfs(
+        cumulative_nadcp_2_2022, nadcp_2_2022, nadcp_2_2021
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(write_csv("nadcp_2_2022.csv", icar_processed_dir(), nadcp_2_2022))
-    FMDData._log_try_error(write_csv("nadcp_2_2021.csv", icar_processed_dir(), nadcp_2_2021))
-    FMDData._log_try_error(write_csv("nadcp_2.csv", icar_processed_dir(), nadcp_2))
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_2_2022.csv", icar_processed_dir(), nadcp_2_2022);
+    logger = logger
+)
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_2_2021.csv", icar_processed_dir(), nadcp_2_2021);
+    logger = logger
+)
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_2.csv", icar_processed_dir(), nadcp_2);
+    logger = logger
+)
+
+if filesize(logfile) == 0
+    rm(logfile)
 end
+
 
 #%%
 logfile = joinpath(icar_processed_logdir, "nadcp_1.log")
 logger = FileLogger(logfile)
 
-with_logger(logger) do
+cumulative_nadcp_1_2021 = Try.@? FMDData._log_try_error(
+    load_csv(
+        "clean_2021_Annual-Report_NADCP-1.csv",
+        icar_cleaned_dir()
+    );
+    logger = logger
+)
 
-    cumulative_nadcp_1_2021 = FMDData._log_try_error(
-        load_csv(
-            "clean_2021_Annual-Report_NADCP-1.csv",
-            icar_cleaned_dir()
+nadcp_1_2020 = Try.@? FMDData._log_try_error(
+    load_csv(
+        "clean_2020_Annual-Report_NADCP-1.csv",
+        icar_cleaned_dir()
+    );
+    logger = logger
+)
+
+nadcp_1_2021 = Try.@? FMDData._log_try_error(
+    infer_later_year_values(
+        cumulative_nadcp_1_2021,
+        nadcp_1_2020,
+    );
+    logger = logger
+)
+
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        cumulative_nadcp_1_2021 => OrderedDict(
+            :sample_year => "2020/2021",
+            :report_year => 2021,
+            :round_name => "NADCP 1",
+            :test_type => "SPCE",
+            :test_threshold => "1.8 log10 @ 50% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    nadcp_1_2020 = FMDData._log_try_error(
-        load_csv(
-            "clean_2020_Annual-Report_NADCP-1.csv",
-            icar_cleaned_dir()
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        nadcp_1_2021 => OrderedDict(
+            :sample_year => 2021,
+            :report_year => 2021,
+            :round_name => "NADCP 1",
+            :test_type => "SPCE",
+            :test_threshold => "1.8 log10 @ 50% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    nadcp_1_2021 = FMDData._log_try_error(
-        infer_later_year_values(
-            cumulative_nadcp_1_2021,
-            nadcp_1_2020,
+Try.@? FMDData._log_try_error(
+    add_all_metadata!(
+        nadcp_1_2020 => OrderedDict(
+            :sample_year => 2020,
+            :report_year => 2020,
+            :round_name => "NADCP 1",
+            :test_type => "SPCE",
+            :test_threshold => "1.8 log10 @ 50% inhibition"
         )
-    )
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            cumulative_nadcp_1_2021 => OrderedDict(
-                :sample_year => "2020/2021",
-                :report_year => 2021,
-                :round_name => "NADCP 1",
-                :test_type => "SPCE",
-                :test_threshold => "1.8 log10 @ 50% inhibition"
-            )
-        )
-    )
+nadcp_1 = Try.@? FMDData._log_try_error(
+    combine_round_dfs(
+        cumulative_nadcp_1_2021, nadcp_1_2021, nadcp_1_2020
+    );
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            nadcp_1_2021 => OrderedDict(
-                :sample_year => 2021,
-                :report_year => 2021,
-                :round_name => "NADCP 1",
-                :test_type => "SPCE",
-                :test_threshold => "1.8 log10 @ 50% inhibition"
-            )
-        )
-    )
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_1_2021.csv", icar_processed_dir(), nadcp_1_2021);
+    logger = logger
+)
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_1_2020.csv", icar_processed_dir(), nadcp_1_2020);
+    logger = logger
+)
+Try.@? FMDData._log_try_error(
+    write_csv("nadcp_1.csv", icar_processed_dir(), nadcp_1);
+    logger = logger
+)
 
-    FMDData._log_try_error(
-        add_all_metadata!(
-            nadcp_1_2020 => OrderedDict(
-                :sample_year => 2020,
-                :report_year => 2020,
-                :round_name => "NADCP 1",
-                :test_type => "SPCE",
-                :test_threshold => "1.8 log10 @ 50% inhibition"
-            )
-        )
-    )
-
-    nadcp_1 = FMDData._log_try_error(
-        combine_round_dfs(
-            cumulative_nadcp_1_2021, nadcp_1_2021, nadcp_1_2020
-        )
-    )
-
-    FMDData._log_try_error(
-        write_csv("nadcp_1_2021.csv", icar_processed_dir(), nadcp_1_2021)
-    )
-    FMDData._log_try_error(
-        write_csv("nadcp_1_2020.csv", icar_processed_dir(), nadcp_1_2020)
-    )
-    FMDData._log_try_error(
-        write_csv("nadcp_1.csv", icar_processed_dir(), nadcp_1)
-    )
+if filesize(logfile) == 0
+    rm(logfile)
 end
